3972d100768301707cdebb7bf98eb3323db0ffb3
yuny0623 <overman522@gmail.com>
2022-04-07 13:24:42 +0900

    first commit

---
+++ src/test/java/mozi/mozispring/MoziSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mozi.mozispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoziSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
