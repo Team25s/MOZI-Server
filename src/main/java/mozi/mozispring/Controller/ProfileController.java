3e5700ea0c53a83bf20bde6250b24dd68a41717c
yuny0623 <overman522@gmail.com>
2022-04-07 17:15:35 +0900

    Add: firebase Service 추가

--- src/main/java/mozi/mozispring/Controller/ProfileController.java
+++ src/main/java/mozi/mozispring/Controller/ProfileController.java
@@ -1,7 +1,20 @@
 package mozi.mozispring.Controller;
 
+import mozi.mozispring.Util.BasicResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProfileController {
+
+    /**
+     * 홈 프로필 편집 api
+     */
+//    @PostMapping("/profile")
+//    @ResponseBody
+//    public ResponseEntity<? extends BasicResponse> updateProfileController(){
+//
+//    }
 }
---
+++ src/main/java/mozi/mozispring/Service/FireBaseInitializer.java
@@ -0,0 +1,4 @@
+package mozi.mozispring.Service;
+
+public class FireBaseInitializer {
+}
