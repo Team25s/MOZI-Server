1745575b6cc2a073e1a5fdc006eb22826481efbf
yuny0623 <overman522@gmail.com>
2022-05-05 14:33:53 +0900

    Delete: test 삭제

--- src/test/java/mozi/mozispring/MoziSpringApplicationTests.java
+++ src/test/java/mozi/mozispring/MoziSpringApplicationTests.java
@@ -1,13 +1,13 @@
-package mozi.mozispring;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MoziSpringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package mozi.mozispring;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MoziSpringApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
