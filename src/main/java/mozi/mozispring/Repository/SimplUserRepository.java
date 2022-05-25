3d863bdce29cea5de2dd5be61c395a550d5ad7a7
yuny0623 <overman522@gmail.com>
2022-05-25 13:28:47 +0900

    Fix: 회원탈퇴 api 수정

--- src/main/java/mozi/mozispring/Repository/SimplUserRepository.java
+++ src/main/java/mozi/mozispring/Repository/SimplUserRepository.java
@@ -4,10 +4,12 @@ import mozi.mozispring.Domain.SimplUser;
 import mozi.mozispring.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface SimplUserRepository  extends JpaRepository<SimplUser, Long> {
 
     SimplUser save(SimplUser simplUser);
