9cf901ea70c6f4ca8cbb5929252372504fe500ce
yuny0623 <overman522@gmail.com>
2022-05-28 14:23:53 +0900

    Add: Favorites 즐겨찾기 기능 추가

--- src/main/java/mozi/mozispring/Favorites/FavoritesController.java
+++ src/main/java/mozi/mozispring/Favorites/FavoritesController.java
@@ -1,7 +1,52 @@
 package mozi.mozispring.Favorites;
 
+import mozi.mozispring.Domain.Dto.DeleteDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class FavoritesController {
+    private final FavoritesRepository favoritesRepository;
+
+    @Autowired
+    public FavoritesController(FavoritesRepository favoritesRepository) {
+        this.favoritesRepository = favoritesRepository;
+    }
+
+    /**
+     * 내 츨겨찾기 목록 반환
+     */
+    @GetMapping("/favorites")
+    @ResponseBody
+    public void getFavoritesController(){
+
+    }
+
+    /**
+        즐겨찾기 등록
+     */
+    @PostMapping("/favorites")
+    @ResponseBody
+    public void registerFavoritesController(){
+
+    }
+
+    /**
+     즐겨찾기 수정
+     */
+    @PutMapping("/favorites")
+    @ResponseBody
+    public void updateFavoritesController(){
+
+    }
+
+    /**
+     즐겨찾기 삭제
+     */
+    @DeleteMapping("/favorites")
+    @ResponseBody
+    public DeleteDto deleteFavoritesController(){
+
+    }
 }
--- src/main/java/mozi/mozispring/Favorites/FavoritesRepository.java
+++ src/main/java/mozi/mozispring/Favorites/FavoritesRepository.java
@@ -1,7 +1,18 @@
 package mozi.mozispring.Favorites;
 
+import mozi.mozispring.Domain.Favorites;
+import mozi.mozispring.Domain.Friend;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class FavoritesRepository {
+public interface FavoritesRepository extends JpaRepository<Favorites, Long> {
+    @Override
+    Favorites save(Favorites favorites);
+    List<Favorites> findAllByUserId(Long userId);
+    Favorites findByUserIdAndOpponentId(Long userId, Long opponentId);
+    void deleteByUserIdAndOpponentId(Long userId, Long opponentId);
 }
+
