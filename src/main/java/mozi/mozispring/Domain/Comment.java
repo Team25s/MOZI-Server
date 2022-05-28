b0c4145d74d6e0fa2a567c30d082c99ddae19f54
yuny0623 <overman522@gmail.com>
2022-05-28 13:33:49 +0900

    Add: swagger annotation 추가

--- src/main/java/mozi/mozispring/Domain/Comment.java
+++ src/main/java/mozi/mozispring/Domain/Comment.java
@@ -22,8 +22,10 @@ public class Comment {       // 댓글 도메인 객체
     @GeneratedValue
     @ApiModelProperty(value = "댓글 id", notes = "Comment의 데이터베이스 private key입니다.", required = true, example="5")
     private Long id;         // 디비 pk
+
     @ApiModelProperty(value = "댓글이 달린 유저의 id", notes = "댓글이 달린 유저의 id", required = true, example="8")
     private Long userId;     // 댓글이 달린 유저의 id
+
     @ApiModelProperty(value = "댓글 내용", notes = "댓글 내용", required = true, example="잘 구경하다 갑니다.")
     private String content;  // 댓글 내용
 }
--- src/main/java/mozi/mozispring/Domain/Dto/MomentRetDto.java
+++ src/main/java/mozi/mozispring/Domain/Dto/MomentRetDto.java
@@ -16,16 +16,21 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MomentRetDto {
-    @ApiModelProperty(value = "댓글 id", notes = "Comment의 데이터베이스 private key입니다.", required = true, example="5")
+    @ApiModelProperty(value = "모먼트 제목", notes = "모먼트 제목", required = true, example="정기모임")
     private String title;
-    @ApiModelProperty(value = "댓글 id", notes = "Comment의 데이터베이스 private key입니다.", required = true, example="5")
+
+    @ApiModelProperty(value = "모먼트 내용", notes = "모먼트 내용", required = true, example="5")
     private String content;
-    @ApiModelProperty(value = "댓글 id", notes = "Comment의 데이터베이스 private key입니다.", required = true, example="5")
+
+    @ApiModelProperty(value = "날짜", notes = "날짜", required = true, example="2022-05-28")
     private String date;
-    @ApiModelProperty(value = "댓글 id", notes = "Comment의 데이터베이스 private key입니다.", required = true, example="5")
+
+    @ApiModelProperty(value = "작성자 id", notes = "작성자 id", required = true, example="45")
     private Long userId;
-    @ApiModelProperty(value = "댓글 id", notes = "Comment의 데이터베이스 private key입니다.", required = true, example="5")
+
+    @ApiModelProperty(value = "해시태그", notes = "해시태그", required = true, example="#한강#여름#친구랑#방학#저녁#카페")
     private String hashTag;
-    @ApiModelProperty(value = "댓글 id", notes = "Comment의 데이터베이스 private key입니다.", required = true, example="5")
+
+    @ApiModelProperty(value = "첨부 파일 이름 리스트", notes = "첨부 파일 이름 리스트", required = true, example="")
     private List<String> fileNameList = new ArrayList<>();
 }
