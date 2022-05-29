2954cae39c6445d09450e72fb358dd1bf08775c8
yuny0623 <overman522@gmail.com>
2022-05-29 18:11:16 +0900

    Fix: controller 에서 service로 로직 분리

--- src/main/java/mozi/mozispring/Profile/ProfileController.java
+++ src/main/java/mozi/mozispring/Profile/ProfileController.java
@@ -25,31 +25,25 @@ public class ProfileController {
     private final UserRepository userRepository;
     private final FireBaseService fireBaseService;
     private final SimplUserRepository simplUserRepository;
+    private final ProfileService profileService;
+
 
     @Autowired
-    public ProfileController(UserRepository userRepository, FireBaseService fireBaseService, SimplUserRepository simplUserRepository) {
+    public ProfileController(UserRepository userRepository, FireBaseService fireBaseService, SimplUserRepository simplUserRepository, ProfileService profileService) {
         this.userRepository = userRepository;
         this.fireBaseService = fireBaseService;
         this.simplUserRepository = simplUserRepository;
+        this.profileService = profileService;
     }
 
     /**
      *  유저 프로필 불러오기
      */
     @ApiOperation(value="유저 프로필 불러오기", notes="유저 프로필 불러오기")
-    @GetMapping("/profile")
+    @GetMapping("/profile/{id}")
     @ResponseBody
-    public ProfileDto getUserProfileController(Long id){
-        Optional<User> findUser = userRepository.findById(id);
-        User user = findUser.get();
-        ProfileDto profileDto = new ProfileDto();
-        profileDto.setProfileFileName(user.getProfileFilename()); // 프로필 이미지 이름
-        profileDto.setProfileFileURL(user.getProfileFileURL());   // 프로필 이미지 링크
-        profileDto.setName(user.getName());
-        profileDto.setMbti(user.getMbti());
-        profileDto.setIntroduce(user.getIntroduce());
-        profileDto.setStrTag(user.getStrTag());
-        return profileDto;
+    public ProfileDto getUserProfileController(@PathVariable("id") Long id){
+        return profileService.getUserProfile(id); // 유저 프로필 부러오기
     }
 
     /**
@@ -63,40 +57,6 @@ public class ProfileController {
         UserDetails userDetails = (UserDetails)principal;
         String userEmail = ((UserDetails) principal).getUsername();
         Optional<User> findUser = userRepository.findByEmail(userEmail);
-
-        User user = findUser.get();
-        user.setName(profileFixDto.getName());
-        user.setIntroduce(profileFixDto.getIntroduce());
-        user.setStrTag(profileFixDto.getStrTag());
-        user.setMbti(profileFixDto.getMbti());
-
-        // 회원 요약 정보도 함께 수정
-        SimplUser simplUser = simplUserRepository.findById(user.getId()).get();
-        simplUser.setName(profileFixDto.getName());
-        simplUser.setMbti(profileFixDto.getMbti());
-
-        if(!profileFixDto.getMultipartFile().isEmpty()){
-            try {
-                fireBaseService.deleteFiles(user.getProfileFilename()); // 기존에 존재하던 이미지는 삭제함.
-            }catch(StorageException e){
-                System.out.println(e.getMessage());
-            }
-            String nameFile= null;
-            String mediaLink= null;
-            try {
-                nameFile = UUID.randomUUID().toString() + ".jpg";
-                mediaLink = fireBaseService.uploadFiles(profileFixDto.getMultipartFile(), nameFile);   // 이미지의 주소를 반환함.
-            }catch(IOException e){
-                System.out.println(e.getMessage());
-            }
-            user.setProfileFilename(nameFile); // 새로운 이미지 파일명 설정
-            user.setProfileFileURL(mediaLink); // 새로운 이미지 링크 설정
-            simplUser.setProfileFilename(nameFile);
-            simplUser.setProfileFileURL(mediaLink);
-        }
-        User savedUser = userRepository.save(user);                   // 유저 정보 저장
-        SimplUser savedSimplUser = simplUserRepository.save(simplUser);  // 유저 요약 정보 저장
-
-        return savedSimplUser; // 사용자 요약정보를 반환
+        return profileService.updateProfile(profileFixDto, findUser); // 프로필 수정하기
     }
 }
--- src/main/java/mozi/mozispring/Profile/ProfileService.java
+++ src/main/java/mozi/mozispring/Profile/ProfileService.java
@@ -1,7 +1,85 @@
 package mozi.mozispring.Profile;
 
+import com.google.cloud.storage.StorageException;
+import mozi.mozispring.Domain.Dto.ProfileDto;
+import mozi.mozispring.Domain.Dto.ProfileFixDto;
+import mozi.mozispring.Domain.SimplUser;
+import mozi.mozispring.Domain.User;
+import mozi.mozispring.Firebase.FireBaseService;
+import mozi.mozispring.User.SimplUserRepository;
+import mozi.mozispring.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ProfileService {
+    private final UserRepository userRepository;
+    private final SimplUserRepository simplUserRepository;
+    private final FireBaseService fireBaseService;
+
+    public ProfileService(UserRepository userRepository, SimplUserRepository simplUserRepository, FireBaseService fireBaseService) {
+        this.userRepository = userRepository;
+        this.simplUserRepository = simplUserRepository;
+        this.fireBaseService = fireBaseService;
+    }
+
+    /**
+     * 유저 프로필 불러오기
+     */
+    public ProfileDto getUserProfile(Long id) {
+        Optional<User> findUser = userRepository.findById(id);
+        User user = findUser.get();
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setProfileFileName(user.getProfileFilename()); // 프로필 이미지 이름
+        profileDto.setProfileFileURL(user.getProfileFileURL());   // 프로필 이미지 링크
+        profileDto.setName(user.getName());
+        profileDto.setMbti(user.getMbti());
+        profileDto.setIntroduce(user.getIntroduce());
+        profileDto.setStrTag(user.getStrTag());
+        return profileDto;
+    }
+
+    /**
+     * 프로필 수정하기
+     */
+    public SimplUser updateProfile(ProfileFixDto profileFixDto, Optional<User> findUser) {
+        User user = findUser.get();
+        user.setName(profileFixDto.getName());
+        user.setIntroduce(profileFixDto.getIntroduce());
+        user.setStrTag(profileFixDto.getStrTag());
+        user.setMbti(profileFixDto.getMbti());
+
+        // 회원 요약 정보도 함께 수정
+        SimplUser simplUser = simplUserRepository.findById(user.getId()).get();
+        simplUser.setName(profileFixDto.getName());
+        simplUser.setMbti(profileFixDto.getMbti());
+
+        if(!profileFixDto.getMultipartFile().isEmpty()){
+            try {
+                fireBaseService.deleteFiles(user.getProfileFilename()); // 기존에 존재하던 이미지는 삭제함.
+            }catch(StorageException e){
+                System.out.println(e.getMessage());
+            }
+            String nameFile= null;
+            String mediaLink= null;
+            try {
+                nameFile = UUID.randomUUID().toString() + ".jpg";
+                mediaLink = fireBaseService.uploadFiles(profileFixDto.getMultipartFile(), nameFile);   // 이미지의 주소를 반환함.
+            }catch(IOException e){
+                System.out.println(e.getMessage());
+            }
+            user.setProfileFilename(nameFile); // 새로운 이미지 파일명 설정
+            user.setProfileFileURL(mediaLink); // 새로운 이미지 링크 설정
+            simplUser.setProfileFilename(nameFile);
+            simplUser.setProfileFileURL(mediaLink);
+        }
+        User savedUser = userRepository.save(user);                   // 유저 정보 저장
+        SimplUser savedSimplUser = simplUserRepository.save(simplUser);  // 유저 요약 정보 저장
+
+        return savedSimplUser; // 사용자 요약정보를 반환
+    }
 }
